dabc29f272b2c5dacc8a887126d903512ed59b0f
thoppe <travis.hoppe@gmail.com>
2014-02-20 12:37:08 -0500

    Started new branch to test sqlite3 database commits

---
+++ generate_db.py
@@ -0,0 +1,62 @@
+import sqlite3
+
+conn = sqlite3.connect(':memory:')
+
+def load_template(f_template, **kwargs):
+    template = []
+    with open(f_template) as FIN:
+        for line in FIN:
+            line = line.strip()
+            if line and line[0][0] != "#":
+                template.append(line)
+    return "\n".join(template)
+
+def create_table_cmd(N, template):
+
+    args = {}
+    args["vertices"]  = N
+    args["max_edges"] = N**2
+    args["cols"] = template.format(**args)
+
+    cmd = "CREATE TABLE IF NOT EXISTS graph{vertices} ({cols})"
+    cmd = cmd.format(**args)
+    return cmd
+
+def select_itr(cmd, arraysize=100):
+    itr = conn.execute(cmd)
+    
+    "An interator over the search using chunks"
+    while True:
+        results = itr.fetchmany(arraysize)
+        if not results:         break
+        for result in results:  yield result      
+
+N = 4
+
+f_graph_template = "graph_template.txt"
+template = load_template(f_graph_template)
+
+conn.execute( create_table_cmd(N, template) )
+
+# Test case, generate a few random graphs and try to add them
+import numpy as np
+for _ in xrange(5):
+    A = np.random.randint(0,2,size=N*N).reshape((N,N))
+
+    args = {}
+    args["n_edge"] = A.sum()
+    args["adj"]    = ''.join(map(str, A.ravel().tolist()))
+    keys = ", ".join(args.keys())
+    vals = ", ".join("'%s'"%args[k] for k in args.keys())
+    cmd = "INSERT INTO graph{} ({}) VALUES ({})"
+    cmd = cmd.format(N,keys,vals)
+    conn.execute(cmd)
+
+#conn.commit()
+  
+cmd = "SELECT * from graph4"
+
+for item in select_itr(cmd):
+    print item
+    
+
