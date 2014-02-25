4a0b0aa33e1424f0da7015a3b89b8f1e3838579f
thoppe <travis.hoppe@gmail.com>
2014-02-25 17:21:19 -0500

    Sequence database being finalized - only checks for unique values if needed

--- src/build_sequence.py
+++ src/build_sequence.py
@@ -8,9 +8,9 @@ parser = argparse.ArgumentParser(description=desc)
 parser.add_argument('--chunksize',type=int,
                     help="Entries to compute before insert is called",
                     default=10000)
-parser.add_argument('--max_N',type=int,default=7,
-                    help="Maximum N to compute sequence over")
-parser.add_argument('--skip_unique',default=False,
+parser.add_argument('--max_n',type=int,default=7,
+                    help="Maximum graph size n to compute sequence over")
+parser.add_argument('--skip_table_build',default=False,
                     action="store_true",
                     help="Skip the unique calculation")
 cargs = vars(parser.parse_args())
@@ -26,9 +26,8 @@ f_database_template = "templates/sequence.sql"
 with open(f_database_template) as FIN:
     conn.executescript(FIN.read())
 
-
 graph_conn = {}
-for n in range(1, cargs["max_N"]+1):
+for n in range(1, cargs["max_n"]+1):
     graph_conn[n] = helper_functions.load_graph_database(n)
 
 # Assume that ref's are the same for all DB's
@@ -46,9 +45,13 @@ INSERT or REPLACE INTO invariant_integer_unique (invariant_id, unique_value)
 VALUES (?,?) 
 '''
 
-if not cargs["skip_unique"]:
+cmd_check_unique = '''SELECT max_n FROM ref_invariant_integer_unique'''
+unique_max_n = max(zip(*conn.execute(cmd_check_unique).fetchall())[0])
+
+if unique_max_n < cargs["max_n"]:
 
     for idx, function_name in invariant_list:
+        logging.info("Building unique list for %s"%function_name)
         unique_vals = set()
         for n in graph_conn:
             q = graph_conn[n].execute(cmd_unique.format(idx))        
@@ -57,10 +60,15 @@ if not cargs["skip_unique"]:
 
         insert_vals = zip(itertools.cycle([idx]), unique_vals)
         conn.executemany(cmd_insert, insert_vals)
+
+    conn.execute("INSERT INTO ref_invariant_integer_unique (max_n) VALUES (?)",
+                 (cargs["max_n"],))
         
     conn.commit()
 
+
 # First steps...
+# Build a list of possible queries and find out which haven't been done
 # Run a basic query over each invariant matching an exact value
 
 cmd_find_unique = '''
@@ -68,7 +76,42 @@ SELECT unique_value FROM invariant_integer_unique WHERE invariant_id={}'''
 
 cmd_count = '''
 SELECT COUNT(*) FROM invariant_integer 
-WHERE invariant_id={} AND value={}'''
+WHERE invariant_id={} AND value={}'''.strip()
+
+cmd_insert_new_sequence = '''INSERT or IGNORE INTO 
+invariant_integer_sequence (query) VALUES ("{}")'''
+
+for idx, function_name in invariant_list:
+    unique_vals = conn.execute(cmd_find_unique.format(idx)).fetchall()
+    for x in unique_vals:
+        for n in graph_conn:
+            q = cmd_count.format(idx, x[0])
+            conn.execute(cmd_insert_new_sequence.format(q))
+conn.commit()
+
+# Find the missing sequences that have max_n < the current max_n
+
+cmd_search = '''
+SELECT seq_id,query FROM invariant_integer_sequence 
+WHERE max_n < {max_n} OR seq IS NULL
+'''
+
+cmd_record_seq = '''
+UPDATE invariant_integer_sequence
+SET max_n=(?), seq=(?) WHERE seq_id=(?)'''
+
+for seq_id,q_text in conn.execute(cmd_search.format(**cargs)):
+    seq = [graph_conn[n].execute(q_text).fetchone()[0] for n in graph_conn]
+    seq_text = str(seq)[1:-1].replace(' ','')
+
+    vals = (cargs["max_n"], seq_text, seq_id)
+    conn.execute(cmd_record_seq, vals)
+    print vals
+
+conn.commit()
+
+           
+exit()
 
 for idx, function_name in invariant_list:
     unique_vals = conn.execute(cmd_find_unique.format(idx)).fetchall()
--- templates/sequence.sql
+++ templates/sequence.sql
@@ -1,7 +1,25 @@
 -- Base sequence values
 
+CREATE TABLE IF NOT EXISTS ref_invariant_integer_unique(
+    max_n        INTEGER DEFAULT 0    
+);
+
+-- Default arguments
+INSERT INTO ref_invariant_integer_unique (max_n) VALUES (0);
+
+
 CREATE TABLE IF NOT EXISTS invariant_integer_unique(
     invariant_id INTEGER,
     unique_value INTEGER,
     PRIMARY KEY (invariant_id, unique_value)
 );
+
+CREATE TABLE IF NOT EXISTS invariant_integer_sequence(
+    seq_id    INTEGER PRIMARY KEY AUTOINCREMENT,   
+    query     TEXT,
+    seq       TEXT NULL,  
+    max_n     INTEGER DEFAULT 0,  
+    OEIS_search TEXT NULL,
+    UNIQUE(query)
+);
+
