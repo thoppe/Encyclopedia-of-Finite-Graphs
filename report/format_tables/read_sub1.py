3bc67de8fd3a7f55e3b8edaaf719a5a4616562d8
thoppe <travis.hoppe@gmail.com>
2014-07-05 21:13:50 -0400

    Added primary subgraphs to paper

--- report/format_tables/read_sub1.py
+++ report/format_tables/read_sub1.py
@@ -12,6 +12,13 @@ def split_name(name):
         if name[n] in "=<>":
             return name[:n], name[n:]
 
+def remove_numbers(name):
+    word_names = {1:"one",2:"two",3:"three",4:"four",5:"five",6:"six",
+                    7:"seven",8:"eight",9:"nine",10:"ten"}
+    for k,v in word_names.items():
+        name = name.replace(str(k),v)
+    return name
+        
 
 with open(f) as FIN:
     for line in FIN:
@@ -22,9 +29,18 @@ with open(f) as FIN:
                 OEIS = "\\OEIS{%s}" % terms[1]
                 raw_name = remove_extra(terms[3])
                 name, cond = split_name(raw_name)
+                name = remove_numbers(name)
+
+                # Modify what we are trying to display
+                if "subgraphfree" in name:
+                    if cond=="=0": cond = ">0"
+                    elif cond=="=1": cond = "=0"
+                        
+                
                 name = "$\\VAR%s %s$" % (name,cond)
 
                 output = [OEIS, name]
+                
 
                 seq = terms[5]
                 seq = seq.replace(',',' ').split()
