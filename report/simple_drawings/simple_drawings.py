4e1089d22e5a3eb2a78d1676cb619370843ff3c7
thoppe <travis.hoppe@gmail.com>
2014-07-11 18:28:40 -0400

    Added some nicer symbols.

--- report/simple_drawings/simple_drawings.py
+++ report/simple_drawings/simple_drawings.py
@@ -2,6 +2,7 @@ import itertools
 import graph_tool as gt
 import graph_tool.draw
 import src.invariants as inv
+import numpy as np
 
 # Combines disjoint graphs
 def add_to(g,a):
@@ -13,23 +14,35 @@ def add_to(g,a):
         g.add_edge(i+offset, j+offset)
 
 g = gt.Graph(directed=False)    
-add_to(g,inv._bowtie_graph)
-add_to(g,inv._diamond_graph)
-add_to(g,inv._open_bowtie_graph)
-add_to(g,inv._bull_graph)
+
+input_g = [inv._diamond_graph,
+           inv._bowtie_graph,
+           inv._open_bowtie_graph,
+           inv._bull_graph]
+
+for h in input_g:
+    add_to(g,h)
+
+pos = gt.draw.sfdp_layout(g, cooling_step=0.99)
+
+
+'''
+counter = 0
+spacing = 3
+
+for hn, h in enumerate(input_g):
+    h_pos = gt.draw.sfdp_layout(h, cooling_step=0.99)
+    hp = np.array([h_pos[x] for x in h.vertices()])
+    hp -= hp.mean(axis=0)
+    
+    for k in h.vertices():       
+        pos[g.vertex(counter)] = hp[int(k)] + [hn*spacing,0]
+        counter += 1
+'''
 
 f_png = "combined_subgraphs.png"
 pos = gt.draw.sfdp_layout(g, cooling_step=0.99)
-help( gt.draw.sfdp_layout )
 
-print pos.a, pos.fa, pos.ma
-exit()
-print pos.get_array()
-help(pos)
+graph_tool.draw.graph_draw(g,pos,output=f_png)
+graph_tool.draw.graph_draw(g,pos)
 
-for n in pos:
-    print n
-
-for n in g.vertices():
-    print dir(g.vertex(n))
-#graph_tool.draw.graph_draw(g,pos,output=f_png)
