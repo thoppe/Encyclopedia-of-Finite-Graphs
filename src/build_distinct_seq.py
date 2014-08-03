f541741ee242e5371805738182e2312b8f5dcec0
thoppe <travis.hoppe@gmail.com>
2014-08-03 12:04:21 -0400

    Distinct seq build will now run the operation in a staged file. This prevents out of memoery errors at the expense of disk space.

--- src/build_distinct_seq.py
+++ src/build_distinct_seq.py
@@ -1,10 +1,9 @@
-import sqlite3, logging, argparse, os, collections
+import sqlite3, logging, argparse, os
 import subprocess, itertools, multiprocessing, json
 import numpy as np
 import helper_functions
 from helper_functions import load_graph_database, load_distinct_database
 from helper_functions import load_sql_script, select_itr
-from helper_functions import attach_table, generate_database_name
 from helper_functions import grab_vector, grab_all, grab_scalar
 
 desc   = '''
@@ -12,6 +11,7 @@ Build sequences from distinct counts
 (e.g. number of automorhism groups for a given size n.'''
 parser = argparse.ArgumentParser(description=desc)
 parser.add_argument('N', type=int)
+parser.add_argument('-f','--force',default=False,action='store_true')
 cargs = vars(parser.parse_args())
 N = cargs["N"]
 
@@ -32,6 +32,18 @@ cmd_get_id = '''SELECT DISTINCT(graph_id) FROM {}'''
 dconn = load_distinct_database(check_exist=False)
 load_sql_script(dconn, "templates/distinct.sql")
 
+# Create an index on graph_id if it doesn't exist yet!
+cmd_index = '''CREATE INDEX IF NOT EXISTS idx_{} ON {} (graph_id)'''
+for name in distinct_seq_names:
+    sconn.execute(cmd_index.format(name,name))
+sconn.commit()
+
+# If forced, drop the terms previously computed
+cmd_remove = '''DELETE FROM distinct_sequence WHERE N=(?) AND function_name=(?)'''
+if cargs["force"]:
+    for name in distinct_seq_names:
+        dconn.execute(cmd_remove,(N,name))
+
 # Skip terms that have already been computed
 cmd_computed = '''SELECT function_name FROM distinct_sequence WHERE N=(?)'''
 known_terms = set(grab_vector(dconn, cmd_computed, (N,)))
@@ -47,30 +59,37 @@ def build_collection(target_function, target_columns):
     cmd_get_seq = '''
     SELECT {cols} FROM {func} WHERE graph_id=(?) ORDER BY {cols}'''
     cmd_get_seq = cmd_get_seq.format(cols=target_columns, func=target_function)
-    C = collections.Counter()
 
-    def format_collection_query(g):
-        vals = grab_all(sconn,cmd_get_seq,g)
+    def format_collection_query(g_id):
+        vals = grab_all(sconn,cmd_get_seq,g_id)
         return tuple(vals)
 
-    for k,g in enumerate(g_id_itr):
-        if k and k%10000==0:
-            msg = "Computing distinct {}, working on graph {}, unique so far {}"
-            logging.info(msg.format(target_function, k, len(C)))
-        vals = format_collection_query(g)
-        C[vals] += 1
-    return C
+    f_sort = "distinct_sort_{}_{}".format(target_function, N)
+    with open(f_sort,'w') as FOUT:
+        for k,g_id in enumerate(g_id_itr):
+            vals = format_collection_query(g_id)
+            FOUT.write(str(vals)+'\n')
 
+    # Use unix to count
+    msg = "Sorting distinct terms in {} ({})".format(target_function,N)
+    logging.info(msg)
+
+    cmd_count_sorted = "sort -g {} | uniq | wc -l".format(f_sort)
+    count = int(subprocess.check_output(cmd_count_sorted,shell=True))
+
+    if os.path.exists(f_sort):
+        os.remove(f_sort)
+    return count
 
 for target_function, target_columns in distinct_seq_names.items():
 
     msg = "Computing distinct terms in {} ({})".format(target_function,N)
     logging.info(msg)
 
-    C = build_collection(target_function, target_columns)
+    count = build_collection(target_function, target_columns)
     cmd_insert = '''INSERT INTO distinct_sequence 
     (function_name, N, coeff) VALUES ("{}",{},{})'''
-    cmd = cmd_insert.format(target_function, N, len(C))
+    cmd = cmd_insert.format(target_function, N, count)
     dconn.execute(cmd)
     dconn.commit()
         
