6adbb9cbe32f1bd68b37135a57ae0f74a3176bb5
thoppe <travis.hoppe@gmail.com>
2014-08-03 11:07:44 -0400

    Clear invariant now has an option for the specials.

--- src/clear_invariant.py
+++ src/clear_invariant.py
@@ -7,16 +7,13 @@ from helper_functions import load_graph_database, grab_scalar
 desc   = "Remove an invariant calculation from the databases"
 parser = argparse.ArgumentParser(description=desc)
 parser.add_argument('remove_function_list',type=str,nargs="+")
+parser.add_argument('--special',default=False,action='store_true')
 parser.add_argument('--max_n',type=int,default=10,
                     help="Maximum graph size n to remove invariant")
 cargs = vars(parser.parse_args())
 
 # Start the logger
 logging.root.setLevel(logging.INFO)
-
-graph_conn = collections.OrderedDict()
-for n in range(1, cargs["max_n"]+1):
-    graph_conn[n] = load_graph_database(n)
 
 cmd_remove_invariant = '''
 UPDATE invariant_integer 
@@ -26,21 +23,47 @@ cmd_remove_computed = '''
 DELETE FROM computed
 WHERE function_name = "{}" '''
 
+cmd_remove_special_invariant = '''DELETE FROM {}'''
+
 def remove_invariant(name, conn):
     conn.execute(cmd_remove_invariant.format(name,name))
     conn.execute(cmd_remove_computed.format(name))
 
-for n,conn in graph_conn.items():
-    for function_name in cargs["remove_function_list"]:
-        msg = "Starting removal of {} from graph database {}"
-        logging.info(msg.format(function_name, n))
-        remove_invariant(function_name, conn)
-    conn.commit()
+def remove_special_invariant(name, conn):
+    conn.execute(cmd_remove_special_invariant.format(name))
+    conn.execute(cmd_remove_computed.format(name))
 
-# Remove the sequence database as it may be corrupted (will have to rebuild)
-f_sequence = os.path.join("database","sequence.db")
-if os.path.exists(f_sequence):
-    os.remove(f_sequence)
-    logging.warning("Removed {}. Please rebuild.".format(f_sequence))
+if not cargs["special"]:
 
+    graph_conn = collections.OrderedDict()
+    for n in range(1, cargs["max_n"]+1):
+        graph_conn[n] = load_graph_database(n)
+
+    for n,conn in graph_conn.items():
+        for function_name in cargs["remove_function_list"]:
+            msg = "Starting removal of {} from graph database {}"
+            logging.info(msg.format(function_name, n))
+            remove_invariant(function_name, conn)
+        conn.commit()
+
+    # Remove the sequence database as it may be corrupted (will have to rebuild)
+    f_sequence = os.path.join("database","sequence.db")
+    if os.path.exists(f_sequence):
+        os.remove(f_sequence)
+        logging.warning("Removed {}. Please rebuild.".format(f_sequence))
+
+elif cargs["special"]:
+
+    graph_conn = collections.OrderedDict()
+    for n in range(1, cargs["max_n"]+1):
+        graph_conn[n] = load_graph_database(n,special=True)
+
+    for n,conn in graph_conn.items():
+        for function_name in cargs["remove_function_list"]:
+            msg = "Starting removal of {} from graph database {}"
+            logging.info(msg.format(function_name, n))
+            remove_special_invariant(function_name, conn)
+        conn.commit()
+    
+    
 
