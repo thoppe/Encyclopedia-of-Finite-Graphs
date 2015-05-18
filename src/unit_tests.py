0822cbf3b0dd341a51ded129d12b745b24a61159
thoppe <travis.hoppe@gmail.com>
2015-05-18 14:41:05 -0400

    Added K4 unit test

--- src/unit_tests.py
+++ src/unit_tests.py
@@ -2,6 +2,22 @@ import unittest
 import invariants
 import networkx as nx
 import numpy as np
+import logging
+# Start the logger
+# logging.root.setLevel(logging.INFO)
+
+
+K4_prop = {
+    "n_vertex": 4,
+    "n_edge": 6,
+    "automorphism_group_n": 24,
+    "chromatic_number": 4,
+    "diameter": 1,
+    "is_strongly_regular": True,
+    "radius": 1,
+    "girth": 3,
+    "n_endpoints": 0,
+}
 
 petersen_prop = {
     "n_vertex": 10,
@@ -29,70 +45,25 @@ petersen_prop = {
     "is_chordal": False,
 }
 
-import logging
-# Start the logger
+############################################################################################
 
-# logging.root.setLevel(logging.INFO)
-logging.info("Testing the Petersen graph for all the invariants")
+#g = nx.petersen_graph()
+#test_prop = petersen_prop
 
-logging.info("Creating the graph.")
-g = nx.petersen_graph()
+g = nx.complete_graph(4)
+test_prop = K4_prop
 
-logging.info("Converting to adj. format")
-adj = invariants.convert_nx_to_adj(g)
+############################################################################################
 
-N = 10
-args = {"N": N}
-
-logging.info("Computing the Tutte polynomial")
-p = invariants.special_polynomial_tutte(adj, **args)
-args["tutte_polynomial"] = p
-
-logging.info("Computing the chromatic polynomial")
-p = invariants.special_chromatic_polynomial(adj, **args)
-args["chromatic_polynomial"] = p
-
-logging.info("Computing the degree sequence")
-p = invariants.special_degree_sequence(adj, **args)
-args["degree_sequence"] = np.array(p).ravel()
-
-logging.info("Computing the fractional chromatic number")
-p = invariants.fractional_chromatic_number(adj, **args)
-args["fractional_chromatic_number"] = p[0]
-
-logging.info("Computing the characteristic polynomial")
-p = invariants.special_characteristic_polynomial(adj, **args)
-args["characteristic_polynomial"] = p
-
-logging.info("Computing the laplacian polynomial")
-p = invariants.special_laplacian_polynomial(adj, **args)
-args["laplacian_polynomial"] = p
-
-logging.info("Computing the independent vertex sets")
-p = invariants.special_independent_vertex_sets(adj, **args)
-args["independent_vertex_sets"] = p
-
-logging.info("Computing the independent edge sets")
-p = invariants.special_independent_edge_sets(adj, **args)
-args["independent_edge_sets"] = p
-
-logging.info("Computing the cycle basis")
-import collections
-p = invariants.special_cycle_basis(adj, **args)
-cb = collections.defaultdict(list)
-for cycle_k, idx in p:
-    cb[cycle_k].append(idx)
-args["cycle_basis"] = cb.values()
 
 ''' TestMaker from StackOverflow: http://stackoverflow.com/a/25267641/249341 '''
-
-
 class TestMaker(type):
 
     def __new__(cls, clsname, bases, dct):
         # Add a method to the class' __dict__ for every key in
-        # the petersen_prop dict.
-        for prop in petersen_prop:
+        # the test_prop dict.
+
+        for prop in test_prop:
             dct['test_{}'.format(prop)] = cls.make_test(prop)
 
         return super(TestMaker, cls).__new__(cls, clsname, bases, dct)
@@ -104,16 +75,63 @@ class TestMaker(type):
         def test_wrap(self):
             func = getattr(invariants, prop)
             self.assertEqual(func(self.adj, **self.args),
-                             petersen_prop[prop])
+                             test_prop[prop])
         return test_wrap
 
 
-class PetersenGraph(unittest.TestCase):
+class GraphTest(unittest.TestCase):
     __metaclass__ = TestMaker
 
     def setUp(self):
+
+        logging.info("Converting to adj. format")
+        adj = invariants.convert_nx_to_adj(g)
+
+        args = {"N": len(g)}
+
+        logging.info("Computing the Tutte polynomial")
+        p = invariants.special_polynomial_tutte(adj, **args)
+        args["tutte_polynomial"] = p
+
+        logging.info("Computing the chromatic polynomial")
+        p = invariants.special_chromatic_polynomial(adj, **args)
+        args["chromatic_polynomial"] = p
+
+        logging.info("Computing the degree sequence")
+        p = invariants.special_degree_sequence(adj, **args)
+        args["degree_sequence"] = np.array(p).ravel()
+
+        logging.info("Computing the fractional chromatic number")
+        p = invariants.fractional_chromatic_number(adj, **args)
+        args["fractional_chromatic_number"] = p[0]
+
+        logging.info("Computing the characteristic polynomial")
+        p = invariants.special_characteristic_polynomial(adj, **args)
+        args["characteristic_polynomial"] = p
+
+        logging.info("Computing the laplacian polynomial")
+        p = invariants.special_laplacian_polynomial(adj, **args)
+        args["laplacian_polynomial"] = p
+
+        logging.info("Computing the independent vertex sets")
+        p = invariants.special_independent_vertex_sets(adj, **args)
+        args["independent_vertex_sets"] = p
+
+        logging.info("Computing the independent edge sets")
+        p = invariants.special_independent_edge_sets(adj, **args)
+        args["independent_edge_sets"] = p
+
+        logging.info("Computing the cycle basis")
+        import collections
+        p = invariants.special_cycle_basis(adj, **args)
+        cb = collections.defaultdict(list)
+        for cycle_k, idx in p:
+            cb[cycle_k].append(idx)
+        args["cycle_basis"] = cb.values()
+    
         self.args = args
         self.adj = adj
+
 
 
 unittest.main(verbosity=2)
