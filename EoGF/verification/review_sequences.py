ce638685f4b0ffdf3061234a00003a5fe35d2a69
thoppe <travis.hoppe@gmail.com>
2015-07-29 11:34:14 -0400

    Review program has been started.

--- EoGF/verification/review_sequences.py
+++ EoGF/verification/review_sequences.py
@@ -2,6 +2,9 @@ import logging
 import argparse
 import json
 import sqlite3
+import requests
+import bs4
+import webbrowser
 
 # Allow a local import to parent directory from a non-package
 from os import sys, path
@@ -28,7 +31,8 @@ cargs.update(options)
 f_seq_info = "sequence_info.sqlite"
 conn = sqlite3.connect(f_seq_info)
 
-columns = "graph_types","invariant_name","invariant_type","cardinality_value","computer_description","OEIS_ref","sequence","unprocessed_flag"
+columns = ("graph_types","invariant_name","invariant_type","cardinality_value",
+           "computer_description","OEIS_ref","sequence","unprocessed_flag")
 
 cmd_select_new = '''
 SELECT {} FROM sequence_info
@@ -38,6 +42,52 @@ invariant_type=(:invariant_type) AND
 unprocessed_flag=1
 '''.format(','.join(columns))
 
+cmd_ignore = '''
+UPDATE sequence_info
+SET unprocessed_flag=0
+WHERE
+graph_types=(:graph_types) AND
+invariant_type=(:invariant_type) AND
+invariant_name=(:invariant_name) AND
+cardinality_value=(:cardinality_value)
+'''
+
+cmd_save = '''
+UPDATE sequence_info
+SET
+unprocessed_flag=0,
+OEIS_ref=(:OEIS_ref)
+WHERE
+graph_types=(:graph_types) AND
+invariant_type=(:invariant_type) AND
+invariant_name=(:invariant_name) AND
+cardinality_value=(:cardinality_value)
+'''
+
+
+def query_OEIS(sequence):
+    url = "http://oeis.org/search"
+    params = {"q":sequence,"fmt":""}
+    r = requests.get(url,params=params)
+    webbrowser.open(r.url,new=0)
+    
+    soup = bs4.BeautifulSoup(r.text)
+    
+    OEIS_ref = ""
+    for link in soup.findAll("a"):
+        if link["href"][:2] == "/A" and link.text.strip()[0] == "A":
+            OEIS_ref = link.text.strip()
+
+    print "Press ENTER for yes, i for ignore, s for skip."
+    msg = "Tag sequence found in OEIS as [{}]?\n".format(OEIS_ref)
+    response = raw_input(msg).lower().strip()
+
+    if not response:
+        item["OEIS_ref"] = OEIS_ref
+
+    return response
+
+
 query_args = cargs.copy()
 query_args["invariant_type"] = "distinct"
 cursor = conn.execute(cmd_select_new,query_args)
@@ -45,4 +95,17 @@ cursor = conn.execute(cmd_select_new,query_args)
 for data in cursor:
     item = dict(zip(columns,data))
     print json.dumps(item,indent=2)
-    exit()
+    q = query_OEIS(item["sequence"]) 
+    if q == "i":
+        print "Marking ignored"
+        conn.execute(cmd_ignore, item)
+    elif q == "s":
+        print "Skipping sequence"
+    elif not q and item["OEIS_ref"]:
+        print "SAVE"
+        conn.execute(cmd_save, item)
+    else:
+        print "Invalid option, skipping anyways!"
+
+    conn.commit()
+
--- fabfile.py
+++ fabfile.py
@@ -9,7 +9,7 @@ args["force"]   = "-f"
 
 # Don't build the third order sequences by default
 args["second_order_flag"] = ""
-args["third_order_flag"] = "-3"
+args["third_order_flag"] = ""
 
 args["calc_exec"]       = "python EoGF/update_invariants.py"
 args["calc_generate"]   = "python EoGF/update_graphs.py"
